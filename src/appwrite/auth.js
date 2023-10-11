f27278074e5997f3c616ce7e56af2ace42f8323e
Kumar_Aditya(2121841) <18bcs4266@gmail.com>
2023-10-11 23:28:52 +0530

    added auth feature using appWrite (BAAS)

---
+++ src/appwrite/auth.js
@@ -0,0 +1,61 @@
+import config from './config/config'
+import {Client,Account,ID} from 'appwrite'
+
+export class AuthService{
+
+  client = new Client();
+  account;
+
+  constructor(){
+    this.client
+         .setEndpoint(config.appwriteUrl)
+         .setProject(config.appwriteProjectId)
+  }
+
+
+  async createAccount({email,password,name}){
+      try{
+        const userAccount = await this.account.create(ID.unique(),email,password,name); 
+          if(userAccount){
+            //call another method
+            //this is envoke the login method when user will create his/her account it will direct login 
+              this.login({email,password});
+          }
+          else{
+            return userAccount;
+          }
+      }
+      catch(error){
+        throw error;
+      }
+  }
+   async login({email,password}){
+    try{
+        return await this.account.createEmailSession(email,password);
+
+    }catch(error){
+        throw error;
+    }
+  }
+  async getCurrentUser(){
+    try{
+        return await this.account.get();
+    }catch(error){
+        throw error;
+    }
+    return null;
+  }
+  async logout(){
+   try{
+     return await this.account.deleteSessions();
+
+   }
+   catch(error){
+    throw error;
+   }
+  }
+}
+
+const authService = new AuthService();
+
+export default authService;
